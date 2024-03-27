8407dae5055f3c706bc1b672d99f7dfceb16c938
Russ James Tan <james.ttan64@gmail.com>
2024-03-27 18:29:20 -0400

    some progress

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
   import { onDestroy } from 'svelte';
   import Input from './lib/Input.svelte';
-  import {state} from './lib/stores.js'
+  import Game from './lib/Game.svelte';
+  import {state, uName, roomID} from './lib/stores.js';
 
   let stateVal: Number;
+  let p1_name: String;
   $: stateVal;
+  $: p1_name;
+
   const unsub1 = state.subscribe((value)=>{
     stateVal = value;
   })
 
+  /*
+  const unsub2 = uName.subscribe((value)=>{
+    p1_name = value;
+  })*/
+
   onDestroy(()=>{
     unsub1
+    //unsub2
   })
 </script>
 
@@ -23,7 +33,7 @@
       <Input/>
     
     {:else if $state==1}
-      <div>Boop</div>
+      <Game p1_name={$uName} room_id={$roomID}/>
     {/if}
 
 
---
+++ client/src/lib/Game.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    
+    export let p1_name:String;
+    export let room_id:String
+</script>
+
+
+<div>
+    <div>{p1_name}</div>
+    <div>{room_id}</div>
+
+    <canvas>
+
+
+    </canvas>
+
+</div>
+
+
+<style>
+
+canvas{
+    background: #000;
+    height: 80%;
+    width: 80%;
+    display: block
+
+}
+</style>
